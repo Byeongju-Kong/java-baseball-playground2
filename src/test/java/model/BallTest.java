dbe4fdbf51216cc88c02e4519799d1b64e6656e0
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-06 21:00:29 +0900

    refactor : check 메서드 이름 변경
    check -> compare

--- src/test/java/model/BallTest.java
+++ src/test/java/model/BallTest.java
@@ -16,19 +16,19 @@ class BallTest {
 
     @Test
     @DisplayName("같은 값과 다른 위치의 Ball객체를 받아 BallStatus.BALL을 반환한다")
-    void check_Ball() {
+    void compare_Ball() {
         assertThat(userBall.compare(new Ball(2, new BaseballNumber(1)))).isEqualTo(BallStatus.BALL);
     }
 
     @Test
     @DisplayName("같은 값과 같은 위치의 Ball객체를 받아 BallStatus.STRIKE를 반환한다")
-    void check_Strike() {
+    void compare_Strike() {
         assertThat(userBall.compare(new Ball(1, new BaseballNumber(1)))).isEqualTo(BallStatus.STRIKE);
     }
 
     @Test
     @DisplayName("다른 값과 다른 위치의 Ball객체를 받아 BallStatus.NOTHING을 반환한다")
-    void check_Nothing() {
+    void compare_Nothing() {
         assertThat(userBall.compare(new Ball(2, new BaseballNumber(2)))).isEqualTo(BallStatus.NOTHING);
     }
 }
