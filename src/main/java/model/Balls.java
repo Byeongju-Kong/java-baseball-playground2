1184a2d36fb40cd74b783bcc364e9fcec197d05b
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-10 16:17:13 +0900

    feat : Balls 테스트 및 구현

--- src/main/java/model/Balls.java
+++ src/main/java/model/Balls.java
@@ -1,15 +1,15 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Balls {
-    private final Set<Ball> numberBalls;
+    private final List<Ball> numberBalls;
     private static final int NUMBER_OF_BALLS = 3;
 
-    public Balls(Set<Ball> randomBalls) {
+    public Balls(List<Ball> randomBalls) {
         numberBalls = randomBalls;
     }
 
@@ -17,16 +17,24 @@ public class Balls {
         numberBalls = validateInputBalls(inputBalls);
     }
 
-    private Set<Ball> validateInputBalls(String[] inputBalls) {
+    private List<Ball> validateInputBalls(String[] inputBalls) {
         if (inputBalls.length != NUMBER_OF_BALLS) {
             throw new IllegalArgumentException("숫자의 개수가 잘못되었습니다.");
         }
-        if (Arrays.stream(inputBalls).distinct().count() > 0){
+        if (Arrays.stream(inputBalls).distinct().count() != NUMBER_OF_BALLS) {
             throw new IllegalArgumentException("숫자에 중복이 있습니다.");
         }
-        Set<Ball> validatedBalls = new HashSet<>();
+        List<Ball> validatedBalls = new ArrayList<>();
         IntStream.range(0, NUMBER_OF_BALLS)
                 .forEach(index -> validatedBalls.add(new Ball(index, new BaseballNumber(inputBalls[index]))));
         return validatedBalls;
+    }
+
+    public BallStatus compare(Ball ball) {
+        return numberBalls.stream()
+                .map(target -> target.compare(ball))
+                .filter(BallStatus::isNotNothing)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
     }
 }
--- src/test/java/model/BallsTest.java
+++ src/test/java/model/BallsTest.java
@@ -2,7 +2,13 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class BallsTest {
@@ -22,5 +28,21 @@ class BallsTest {
         assertThatThrownBy(() -> new Balls(new String[]{"1", "2", "3", "4"}))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자의 개수가 잘못되었습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("Ball 객체를 받아 옳은 BallStatus를 반환한다")
+    @MethodSource("provideBallAndBallStatus")
+    void compare(Ball ball, BallStatus expected) {
+        Balls balls = new Balls(new String[]{"3", "7", "8"});
+        assertThat(balls.compare(ball)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideBallAndBallStatus() {
+        return Stream.of(
+                Arguments.of(new Ball(0, new BaseballNumber(3)),BallStatus.STRIKE),
+                Arguments.of(new Ball(1, new BaseballNumber(3)), BallStatus.BALL),
+                Arguments.of(new Ball(1, new BaseballNumber(4)), BallStatus.NOTHING)
+        );
     }
 }
