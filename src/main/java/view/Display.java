ccd9c5f154a98dc03b6be35d145b2fdd87f8a839
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-11 16:39:49 +0900

    feat : view/Display 구현

---
+++ src/main/java/view/Display.java
@@ -0,0 +1,28 @@
+package view;
+
+import model.Count;
+
+public class Display {
+    private Display() {
+    }
+
+    public static void alertInput() {
+        System.out.print("숫자를 입력하세요 : ");
+    }
+
+    public static void alertRestartInput() {
+        System.out.print("게임을 재시작하려면 1, 종료하려면 2를 누르세요 : ");
+    }
+
+    public static void alertOver() {
+        System.out.println("게임을 종료합니다.");
+    }
+
+    public static void showResult(Count count) {
+        System.out.println(count.getStrike() + "Strike " + count.getBall() + "Ball");
+    }
+
+    public static void showResult() {
+        System.out.println("Nothing");
+    }
+}
