5c89c94ecb53d345c425ad5ec04c7fc709be8917
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-11 14:06:48 +0900

    feat : Input 구현

---
+++ src/main/java/controller/Input.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.Scanner;
+
+public class Input {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private Input() {
+    }
+
+    public static String[] input() {
+        return SCANNER.nextLine()
+                .split("");
+    }
+}
