d2be99dd38f2f863ff4015c95871ccc5551f5291
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-11 17:40:26 +0900

    Main 구현

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Main {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.run();
+    }
+}
