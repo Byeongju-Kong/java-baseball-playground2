360a0ddd09de67d8fb79476e00cec9c0322a2d8c
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-18 16:05:52 +0900

    refactor : CsvSource로 수정 및 테스트 명료화

--- src/test/java/model/dto/CountDTOTest.java
+++ src/test/java/model/dto/CountDTOTest.java
@@ -3,6 +3,8 @@ package model.dto;
 import model.BallStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,26 +14,27 @@ class CountDTOTest {
     @Test
     @DisplayName("BallStatus.Strike를 받아 Strike 카운트를 1 증가시킨다.")
     void countUpStrike(){
+        int expect = count.getStrike() + 1;
         count.countUp(BallStatus.STRIKE);
-        assertThat(count.getStrike()).isEqualTo(1);
-        count.countUp(BallStatus.STRIKE);
-        assertThat(count.getStrike()).isEqualTo(2);
+        int actual = count.getStrike();
+        assertThat(actual).isEqualTo(expect);
     }
 
     @Test
     @DisplayName("BallStatus.Ball을 받아 Ball 카운트를 1 증가시킨다")
     void countUpBall() {
+        int expect = count.getBall() + 1;
         count.countUp(BallStatus.BALL);
-        assertThat(count.getBall()).isEqualTo(1);
-        count.countUp(BallStatus.BALL);
-        assertThat(count.getBall()).isEqualTo(2);
+        int actual = count.getBall();
+        assertThat(actual).isEqualTo(expect);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"STRIKE,false", "NOTHING,true"})
     @DisplayName("낫씽일 경우를 반환한다")
-    void isNothing() {
+    void isNothing(BallStatus ballStatus, boolean expect) {
         count = new CountDTO();
-        count.countUp(BallStatus.NOTHING);
-        assertThat(count.isNothing()).isTrue();
+        count.countUp(ballStatus);
+        assertThat(count.isNothing()).isEqualTo(expect);
     }
 }
