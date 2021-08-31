34f65b87f6f737973f5d6ee45c620ca1c6cd8d36
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-31 16:01:17 +0900

    refactor : 숫자의 개수 매직 넘버로 치환

--- src/main/java/model/ball/Balls.java
+++ src/main/java/model/ball/Balls.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 
 public class Balls {
     private final List<Ball> numberBalls;
+    private static final int NUMBER_OF_BALLS = 3;
 
     public Balls(final int[] balls) {
         validateInputBalls(balls);
@@ -14,18 +15,17 @@ public class Balls {
     }
 
     private void validateInputBalls(final int[] inputBalls) {
-        int numberOfBalls = 3;
-        if (inputBalls.length != numberOfBalls) {
+        if (inputBalls.length != NUMBER_OF_BALLS) {
             throw new IllegalArgumentException("숫자의 개수가 잘못되었습니다.");
         }
-        if (Arrays.stream(inputBalls).distinct().count() != numberOfBalls) {
+        if (Arrays.stream(inputBalls).distinct().count() != NUMBER_OF_BALLS) {
             throw new IllegalArgumentException("숫자에 중복이 있습니다.");
         }
     }
 
     private List<Ball> parseInputBalls(final int[] inputBalls) {
         List<Ball> validatedBalls = new ArrayList<>();
-        IntStream.range(0, inputBalls.length)
+        IntStream.range(0, NUMBER_OF_BALLS)
                 .forEach(index -> validatedBalls.add(new Ball(index, inputBalls[index])));
         return validatedBalls;
     }
