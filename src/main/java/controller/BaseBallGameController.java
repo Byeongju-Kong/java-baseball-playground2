b7a5e2c69ddd3c37cc4204e1503a1b166f79a8ec
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-14 15:48:04 +0900

    refactor : 랜덤 숫자 생성 시점 변경

--- src/main/java/controller/BaseBallGameController.java
+++ src/main/java/controller/BaseBallGameController.java
@@ -16,16 +16,16 @@ import static view.rounddisplay.RoundDisplays.findRoundDisplayStrategy;
 public class BaseBallGameController{
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private BaseballGame baseballGame;
+    private final BaseballGame baseballGame;
     private final Displayable display;
 
     public BaseBallGameController(final Displayable display) {
         this.display = display;
+        int[] randomNumbers = new RandomNumbers().generateRandomNumbers(MIN_NUMBER, MAX_NUMBER);
+        baseballGame = new BaseballGame(randomNumbers);
     }
 
     public void play() {
-        int[] randomNumbers = new RandomNumbers().generateRandomNumbers(MIN_NUMBER, MAX_NUMBER);
-        baseballGame = new BaseballGame(randomNumbers);
         playWhileNotOver();
         display.alertOver();
     }
