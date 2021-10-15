c67c5990037032a33ce5a2036fe654962e8c3875
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-15 16:28:28 +0900

    refactor : while -> do~while 문으로 변경

--- src/main/java/controller/BaseBallGameController.java
+++ src/main/java/controller/BaseBallGameController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static view.rounddisplay.RoundDisplays.findRoundDisplayStrategy;
 
 
-public class BaseBallGameController{
+public class BaseBallGameController {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
     private final BaseballGame baseballGame;
@@ -31,12 +31,13 @@ public class BaseBallGameController{
     }
 
     private void playWhileNotOver() {
-        while (baseballGame.isNotOver()) {
+        do {
             int[] inputNumbers = new InputNumberDTO(display.input()).getValidatedInput();
             List<BallStatus> ballStatuses = baseballGame.getCount(inputNumbers);
             CountDTO countDTO = new CountDTO(ballStatuses);
             RoundDisplay roundDisplay = findRoundDisplayStrategy(countDTO);
             roundDisplay.showRoundResult(countDTO);
         }
+        while (baseballGame.isNotOver());
     }
 }
